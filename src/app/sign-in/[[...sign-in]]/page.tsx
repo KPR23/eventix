88817de8a97476c34925c08103087ac86edd0e65
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2025-12-02 10:53:15 +0100

    chore: disable automatic import organization, and add styling to the sign-in page.

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignIn />;
+export default function SignInPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="font-science-gothic text-4xl">Sign In</h1>
+      <SignIn />
+    </div>
+  );
 }
